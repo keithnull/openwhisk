954715056ac907e01527c53dd95b0abd4ab83a82
keithnull <keith1126@126.com>
2021-03-31 19:32:17 +0800

    Add priority queue before sending activations to invokers

--- core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/PriorityBalancer.scala
+++ core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/PriorityBalancer.scala
@@ -11,6 +11,16 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import scala.concurrent.Future
 import org.apache.openwhisk.core.connector.ActivationMessage
+// import org.apache.kafka.clients.producer.RecordMetadata
+// import scala.util.{Failure, Success}
+// import akka.event.Logging.InfoLevel
+import java.util.concurrent.PriorityBlockingQueue
+import scala.concurrent.duration.FiniteDuration
+import java.util.concurrent.TimeUnit
+import java.util.Comparator
+import org.apache.openwhisk.core.entity.ActionPriority.High
+import org.apache.openwhisk.core.entity.ActionPriority.Low
+import org.apache.openwhisk.core.entity.ActionPriority.Normal
 
 class PriorityBalancer(config: WhiskConfig, feedFactory: FeedFactory, controllerInstance: ControllerInstanceId)(implicit
   actorSystem: ActorSystem,
@@ -18,16 +28,43 @@ class PriorityBalancer(config: WhiskConfig, feedFactory: FeedFactory, controller
   materializer: ActorMaterializer)
     extends LeanBalancer(config, feedFactory, controllerInstance) {
 
+  protected val priorityQueue =
+    new PriorityBlockingQueue[(ActionPriority, ActivationMessage)](10, new PriorityMessageComparator)
+
   // add priority related code here
   override def publish(action: ExecutableWhiskActionMetaData, msg: ActivationMessage)(implicit
     transid: TransactionId): Future[Future[Either[ActivationId, WhiskActivation]]] = {
 
     val priorityStr = action.annotations.getAs[String](Annotations.ActionPriority).getOrElse("")
     val priority = ActionPriority.fromString(priorityStr)
-    println(s"priority = $priority, action = ${msg.activationId}")
+    logging.info(this, s"action = ${action.name}, priority = $priority, activation = ${msg.activationId}")
     val activationResult = setupActivation(msg, action, invokerName)
-    sendActivationToInvoker(messageProducer, msg, invokerName).map(_ => activationResult)
+    priorityQueue.put((priority, msg))
+    Future(activationResult)
   }
+
+  // create an applier that constantly applies messages in the priority queue
+  private def makeALocalThreadedApplier(duration: FiniteDuration): Unit = {
+    val thread = new Thread {
+      override def run {
+
+        def runOnce(): Unit = {
+          Option(priorityQueue.poll(duration.toMillis, TimeUnit.MILLISECONDS)) match {
+            case Some((p, msg)) => {
+              logging.info(this, s"Execute activation: priority = $p, activation = ${msg.activationId}")
+              sendActivationToInvoker(messageProducer, msg, invokerName)
+            }
+            case None => Unit
+          }
+          runOnce()
+        }
+
+        runOnce()
+      }
+    }
+    thread.start
+  }
+  makeALocalThreadedApplier(FiniteDuration(50, TimeUnit.MILLISECONDS))
 }
 
 object PriorityBalancer extends LoadBalancerProvider {
@@ -43,4 +80,30 @@ object PriorityBalancer extends LoadBalancerProvider {
   def requiredProperties =
     ExecManifest.requiredProperties ++
       wskApiHost
+}
+
+class PriorityMessageComparator extends Serializable with Comparator[(ActionPriority, ActivationMessage)] {
+
+  def compare(x1: (ActionPriority, ActivationMessage), x2: (ActionPriority, ActivationMessage)): Int = {
+    val p1 = x1._1
+    val p2 = x2._1
+    p1 match {
+      case High =>
+        p2 match {
+          case High => 0
+          case _    => 1
+        }
+      case Low =>
+        p2 match {
+          case Low => 0
+          case _   => -1
+        }
+      case Normal =>
+        p2 match {
+          case High   => -1
+          case Normal => 0
+          case Low    => 1
+        }
+    }
+  }
 }
