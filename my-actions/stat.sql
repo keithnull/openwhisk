7b6318025c3a55a406a377d140ffd73ec053e3fe
Keith Null <keith1126@126.com>
2021-04-25 19:18:45 +0800

    Move benchmark scripts outside to their own repo

--- my-actions/stat.sql
+++
@@ -1,8 +0,0 @@
--- SQLite
-SELECT
-    priority,
-    COUNT(*) AS activations_cnt,
-    SUM(started_at - created_at) / COUNT(*) AS average_delay_in_ms
-FROM activations
-GROUP BY priority
-ORDER BY average_delay_in_ms;
